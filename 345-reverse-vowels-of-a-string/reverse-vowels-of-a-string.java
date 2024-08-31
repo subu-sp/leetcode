301aca06e4bfd2362b6d713e9ad80349bdcca4ef
Subhra Praharaj <130889517+subu-sp@users.noreply.github.com>
2024-08-31 23:10:41 +0530

    Time: 5 ms (39.62%) | Memory: 45.6 MB (17.58%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        String vowels = "aeiouAEIOU";
+        
+        while (start < end) {
+            while (start < end && !vowels.contains(chars[start] + "")) { //Using + "" is a concise way to convert a char or other primitive types to a String, like 'a' is converted to the string "a".
+                start++;
+            }
+            
+            while (start < end && !vowels.contains(chars[end] + "")) {
+                end--;
+            }
+            
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            
+            start++;
+            end--;
+        }
+        return new String(chars);
+    
+    }
+}
