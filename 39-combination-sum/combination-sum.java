b23a58b65cdc9b94a29394f28ad3fe87c18b7c95
Subhra Praharaj <130889517+subu-sp@users.noreply.github.com>
2024-09-04 23:34:50 +0530

    Time: 2 ms (83.11%) | Memory: 44.2 MB (94.24%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        gen(0, candidates, target, new ArrayList<>(), res);
+        return res;
+    }
+    private void gen(int i, int[] arr,int target, List<Integer> curr, List<List<Integer>> res){
+            
+          if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        if (target < 0 || i == arr.length) {
+            return;
+        }
+            curr.add(arr[i]);
+            gen(i, arr, target - arr[i], curr, res);
+
+            curr.remove(curr.size()-1);
+            gen(i+1, arr, target, curr, res);
+
+        }
+    
+
+    
+}
